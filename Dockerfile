c0aed79d18a39f900aa76ff392b3d3cdc3f6ddc3
nehal <kadyannehal333@gmail.com>
2025-04-20 16:33:56 +0530

    first commit

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM ubuntu
+
+RUN apt-get update
+
+RUN apt-get install -y curl
+
+RUN curl -sl https://deb.nodesource.com/setup_18.x | bash -
+
+RUN apt-get upgrade -y
+
+RUN apt-get install -y nodejs
+
+COPY package.json package.json
+
+COPY package-lock.json package-lock.json
+ 
+COPY index.js index.js
+
+RUN npm install
+
+ENTRYPOINT [ "node", "index.js" ]
---
+++ index.js
@@ -0,0 +1,18 @@
+const express = new require("express");
+const dotenv = require("dotenv")
+
+dotenv.config()
+
+const app = express();
+
+// port
+
+const port = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+    return res.status(200).send("<h1>Hello from Dockerized app!</h1>")
+})
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`)
+})
